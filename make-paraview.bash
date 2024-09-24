588d18af93d864eae88b40e570bfb557151a61ea
Kenneth Moreland <morelandkd@ornl.gov>
2024-09-23 22:30:34 -0400

    Save the package builds in the spack-build directory
    This lets you look at the build results of successful builds
    to make sure that they built as expected.

--- make-paraview.bash
+++ make-paraview.bash
@@ -5,4 +5,4 @@ scriptdir=$(dirname $(realpath $0))
 . $scriptdir/setup-spack.sh
 
 spack concretize -f
-spack install --add paraview @5.13.0+raytracing+python+mpi+adios2+visitbridge+use_vtkm+rocm amdgpu_target=gfx90a
+spack install --add --keep-stage paraview @5.13.0+raytracing+python+mpi+adios2+visitbridge+rocm amdgpu_target=gfx90a
