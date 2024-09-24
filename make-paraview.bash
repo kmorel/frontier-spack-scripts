08635bacb0ecc472afa3bc094965f1aefeb0b856
Kenneth Moreland <morelandkd@ornl.gov>
2024-09-24 14:51:57 -0400

    Add configuration for paraview server connection

--- make-paraview.bash
+++ make-paraview.bash
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+pv_version=5.13.0
+
 scriptdir=$(dirname $(realpath $0))
 
 . $scriptdir/setup-spack.sh
 
 spack concretize -f
-spack install --add --keep-stage paraview @5.13.0+raytracing+python+mpi+adios2+visitbridge+rocm amdgpu_target=gfx90a
+spack install --add --keep-stage paraview @$pv_version+raytracing+python+mpi+adios2+visitbridge+rocm amdgpu_target=gfx90a
+
+# Make a custom server configuration that points to this build.
+
+# This works best if this build directory has the date in it.
+build_name=frontier-$(basename $scriptdir)-$pv_version
+pvsc_file=$scriptdir/$build_name.pvsc
+
+cp -f $scriptdir/pvsc/ORNL/ORNL-frontier.pvsc $pvsc_file
+
+sed -i "s/ORNL frontier/$build_name/" $pvsc_file
+sed -i "s|/sw/frontier/paraview/pvsc|$scriptdir/pvsc|" $pvsc_file
--- setup-spack.sh
+++ setup-spack.sh
@@ -1,5 +1,14 @@
-myspack_base_dir=`dirname $0`
+if test -n "$BASH" ; then myscript=$BASH_SOURCE
+elif test -n "$TMOUT"; then myscript=${.sh.file}
+elif test -n "$ZSH_NAME" ; then myscript=${(%):-%x}
+elif test ${0##*/} = dash; then x=$(lsof -p $$ -Fn0 | tail -1); myscript=${x#n}
+else script=$0
+fi
+
+myspack_base_dir=`dirname $myscript`
 myspack_base_dir=`realpath $myspack_base_dir`
+
+unset myscript
 
 export SPACK_USER_CONFIG_PATH=$myspack_base_dir/spack-configs/e4s-configs/frontier
 . $myspack_base_dir/spack/share/spack/setup-env.sh
