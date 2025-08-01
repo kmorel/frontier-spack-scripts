0bd8a8d2bd6d2b1beee437736b724db3bbe81c91
Kenneth Moreland <morelandkd@ornl.gov>
2025-08-01 12:55:11 -0400

    Add builds of makeinfo and automake to path
    I kept running into issues where bad file modification times caused
    unexpected things to build. I worked around that by installing the
    necessary programs outside of spack and make them available in my path.

--- make-paraview.bash
+++ make-paraview.bash
@@ -8,6 +8,14 @@ scriptdir=$(dirname $(realpath $0))
 
 . $scriptdir/setup-spack.sh
 
+# We should not need makeinfo or automake, but sometimes mistakes in file
+# modification times causes builds to use them. Add these to the path.
+package_dir=/ccs/home/kmorel/local/frontier/packages
+export PATH=$PATH:$package_dir/texinfo-7.2/bin
+export PATH=$PATH:$package_dir/automake-1.16.5/bin
+export PATH=$PATH:$package_dir/automake-1.17/bin
+export PATH=$PATH:$package_dir/automake-1.18.1/bin
+
 spack concretize -f
 # Need to specifically add automake 1.16 to prevent errors from the automake 1.15
 # installed on frontier.
