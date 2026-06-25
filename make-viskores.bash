643f6e800b3b3589a625a42ecd567f1e90a4801f
Kenneth Moreland <morelandkd@ornl.gov>
2026-06-25 10:38:55 -0400

    Update viskores build to 1.1

--- make-viskores.bash
+++ make-viskores.bash
@@ -5,4 +5,4 @@ scriptdir=$(dirname $(realpath $0))
 . $scriptdir/setup-spack.sh
 
 #spack concretize -f
-spack install --add viskores @1.0.0+rocm~openmp+examples+kokkos+tbb amdgpu_target=gfx90a
+spack install --add viskores @1.1+rocm~openmp+examples+kokkos+tbb amdgpu_target=gfx90a
