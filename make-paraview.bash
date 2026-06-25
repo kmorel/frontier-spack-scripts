9b4c8694ce82ce369c6c043c2d37fa53499cd347
Kenneth Moreland <morelandkd@ornl.gov>
2026-06-25 10:33:53 -0400

    Add ability to compile paraview on compute node
    The `make_paraview.sh` script now has the ability to download source
    files to a mirror, launch itself on a compute node, and compile on
    the compute node. This works OK, but I am not having a lot of success
    compiling very far on the compute node, so I may need to back off of
    this.

--- make-paraview.bash
+++ make-paraview.bash
@@ -1,10 +1,53 @@
 #!/bin/bash
+#SBATCH -A CSC143
+#SBATCH -J pv-compile
+#SBATCH -o %x-%j.log
+#SBATCH -N 1
+#SBATCH -t 02:00:00
+#SBATCH -p batch
+#SBATCH -q normal
+#SBATCH --threads-per-core=1
+#SBATCH --mail-user=morelandkd@ornl.gov
+#SBATCH --mail-type=ALL
 
 set -e
 
 pv_version=6.1.0
 
-scriptdir=$(dirname $(realpath $0))
+use_queue=false
+
+usage() {
+  echo "Usage: $0 [-q|--queue]"
+}
+
+while [ $# -gt 0 ] ; do
+  case "$1" in
+    -q|--queue)
+      use_queue=true
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Unknown argument: $1" >&2
+      usage >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -n "$SLURM_JOB_ID" ] ; then
+  echo "Running in batch job."
+  in_batch=true
+  scriptdir=${SLURM_SUBMIT_DIR}
+else
+  in_batch=false
+  scriptdir=$(dirname $(realpath $0))
+fi
+
+cd ${scriptdir}
 
 . $scriptdir/setup-spack.sh
 
@@ -16,19 +59,41 @@ export PATH=$PATH:$package_dir/automake-1.16.5/bin
 export PATH=$PATH:$package_dir/automake-1.17/bin
 export PATH=$PATH:$package_dir/automake-1.18.1/bin
 
-spack concretize -f
 # Need to specifically add autoconf to prevent errors from reconfigured stuff
 # installed on frontier.
-spack install --add autoconf
+spack add autoconf
+spack install autoconf
 spack load autoconf
-# spack install --add --keep-stage \
-spack install --add \
+
+spack add \
   paraview@$pv_version+raytracing~x~qt+python+mpi+adios2+fides+visitbridge+rocm amdgpu_target=gfx90a \
   ^ospray~volumes \
   ^hwloc~gl \
   ^viskores~openmp \
   ^mgard@git.master+rocm~openmp amdgpu_target=gfx90a \
-  ^adios2@master+rocm+mgard+campaign amdgpu_target=gfx90a
+  ^adios2@master+rocm+mgard+campaign+sodium amdgpu_target=gfx90a
+
+mirrordir=${scriptdir}/spack-mirror
+spack concretize
+if [ "$in_batch" = false ] ; then
+  spack mirror create -d ${mirrordir} -a
+fi
+
+if [ "$use_queue" = true ] ; then
+  echo "Queuing build job"
+  sbatch $0
+  exit 0
+fi
+
+# spack mirror add local file://${mirrordir} # Should already be added.
+# spack install --keep-stage
+if [ "$in_batch" = true ] ; then
+  echo "Running spack install on compute node."
+  srun -n 1 -c 56 spack install
+else
+  echo "Running spack install locally."
+  spack install
+fi
 
 # ParaView indirectly depends on lua, and this overrides the lua
 # that srun uses. srun needs the luaposix library, so load that,
